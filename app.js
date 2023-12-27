04fc2f2314148732f6a744a4c50cd4cfafade46b
sothulthorn <thornsothul@gmail.com>
2023-12-27 21:32:00 +0900

    Create Middleware

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ const app = express();
 // middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   res.status(200).json({
     status: 'success',
+    requestdAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
