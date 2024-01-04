ea42c1f8ef8c2131f43b4fe2cea0f6578ded2594
sothulthorn <thornsothul@gmail.com>
2024-01-04 21:26:57 +0900

    Aggregation Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -92,13 +92,19 @@ tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
 
   this.start = Date.now();
-
   next();
 });
 
-tourSchema.post(/^find/, (docs, next) => {
+tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
   next();
 });
 
