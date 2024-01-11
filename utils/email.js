12f3e963941c559d25995bb5b21427ef0aa3a83c
sothulthorn <thornsothul@gmail.com>
2024-01-11 21:54:04 +0900

    Password Reset Functionality Setting New Password

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async (options) => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'Jame Stepenson <hello@james.io>',
+    // from: 'Jame Stepenson <hello@james.io>',
     to: options.email,
     subject: options.subject,
     text: options.message,
