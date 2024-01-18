7d95035cb1f9d23ecfc98602a4459a1d040a18fa
sothulthorn <thornsothul@gmail.com>
2024-01-18 19:52:37 +0900

    Logging in Users with Our API - Part 1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -35,7 +35,13 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', {
-    title: 'Log into your account',
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.axios.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://axios.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;",
+    )
+    .render('login', {
+      title: 'Log into your account',
+    });
 };
