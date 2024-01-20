36135b3277e195f0f62e15105f0b23e2c6d933d3
sothulthorn <thornsothul@gmail.com>
2024-01-20 16:38:57 +0900

    Uploading Multiple Images Tours

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12309,7 +12309,6 @@ if (userDataForm) {
     form.append('name', document.getElementById('name').value);
     form.append('email', document.getElementById('email').value);
     form.append('photo', document.getElementById('photo').files[0]);
-    console.log(form);
     (0, _updateSettings.updateSettings)(form, 'data');
   });
 }
